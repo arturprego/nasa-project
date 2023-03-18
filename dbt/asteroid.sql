7c534d813ebde29bd5a423d53621e53f5a240c04
arturprego <127404024+arturprego@users.noreply.github.com>
2023-03-18 17:35:53 +0000

    Create asteroid.sql

---
+++ dbt/asteroid.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+select id,
+    name,
+    absolute_magnitude_h,
+    est_dim_min_km,
+    est_dim_max_km,
+    is_potentially_hazardous_asteroid,
+    orbital_period,
+    perihelion_distance,
+    aphelion_distance,
+    orbit_class_type,
+    is_sentry_object
+from {{ ref('stg_asteroids_raw') }}
